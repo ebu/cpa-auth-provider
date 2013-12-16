0499170922f033d560bb8ad3a99c787140713bf8
barroco <barroco@ebu.ch>
2013-12-16 14:16:42 +0100

    Move http require + gitignore

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var app = require('./lib/app');
+var http = require('http');
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'))
