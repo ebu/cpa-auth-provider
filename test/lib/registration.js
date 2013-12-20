8313d000c1978b8352c2c0c3efa7c771dcfd422d
barroco <barroco@ebu.ch>
2013-12-20 14:47:56 +0100

    Fix tests

--- test/lib/registration.js
+++ test/lib/registration.js
@@ -3,7 +3,6 @@
 
 // Test for the dynamic registration end point
 
-
 describe('POST /register', function() {
 
   var correctRequest = {
@@ -13,20 +12,30 @@ describe('POST /register', function() {
   };
 
   context("While sending the Content-Type: ", function() {
-    it('responds with OK and a client_id', function() {
+    it('responds with 201 and a client_id', function(done) {
       request.post('/register').type("application/json").send(JSON.stringify(correctRequest)).end(function(err, res) {
-        expect(res.statusCode).to.equal(201);
-        expect(res.body.client_id).to.be.above(0);
+        if (err) {
+          done(err);
+        } else {
+          expect(res.statusCode).to.equal(201);
+          done();
+        }
       });
     });
   });
 
   context("Without defining the content type", function() {
-    it('returns a 400 Bad request error', function() {
-      request.post('/register').send(correctRequest).end(function(err, res) {
-        expect(res.statusCode).to.equal(400);
+    it('returns a 400 Bad request error', function(done) {
+      request.post('/register').send(JSON.stringify(correctRequest)).end(function(err, res) {
+        if (err) {
+          done(err);
+        } else {
+          expect(res.statusCode).to.equal(400);
+          done();
+        }
       });
     });
   });
+
 
 });
--- test/test-helper.js
+++ test/test-helper.js
@@ -2,11 +2,10 @@ global.chai = require("chai");
 global.expect = chai.expect;
 global.sinon = require("sinon");
 
-sinonChai = require("sinon-chai");
+var sinonChai = require("sinon-chai");
 chai.use(sinonChai);
 
 
 var app = require('../lib/app');
-
-supertest = require("supertest");
+var supertest = require("supertest");
 global.request = supertest(app);
