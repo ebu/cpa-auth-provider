f4e489ad0c16002e14e7ee1cc72b3230b49914c7
barroco <barroco@ebu.ch>
2014-01-08 18:25:59 +0100

    Simple User Code Validation

--- routes/device-profile.js
+++ routes/device-profile.js
@@ -4,6 +4,7 @@ var db = require('../models');
 var generate = require('../lib/generate');
 var verify = require('../lib/verify');
 var config = require('../config');
+var messages = require('../lib/messages');
 
 module.exports = function (app, options) {
 
@@ -56,5 +57,45 @@ module.exports = function (app, options) {
 
   });
 
+  var renderVerificationPage = function(req, res, error_message) {
+    if (typeof error_message === 'string') {
+      res.render('verify.ejs', { 'values': req.body, 'error': error_message });
+    } else {
+      res.render('verify.ejs', { 'values': req.body, 'error': null });
+    }
+
+  };
+
+  app.get('/verify', renderVerificationPage);
+
+  app.post('/verify', function(req, res) {
+    if (req.headers['content-type'] === 'application/x-www-form-urlencoded' && req.body.user_code) {
+
+      var posted_user_code = req.body.user_code;
+
+      db.PairingProcess.find({ where: { 'user_code': posted_user_code }}).success(function(pairingProcess) {
+        if (pairingProcess) {
+
+          if (pairingProcess.verified) {
+            res.send(404);
+          } else {
+            pairingProcess.updateAttributes({verified: true}).success(function() {
+              res.render('verify-success.ejs');
+            }).error(function() {
+              res.send(500);
+            });
+          }
+        } else {
+          renderVerificationPage(req, res, messages.INVALID_USERCODE);
+        }
+      }).error(function(err) {
+        res.send(500);
+      });
+
+    } else {
+      res.send(400);
+    }
+  });
 };
+
 
