a79e0719fb8b954ba674fd60eb4f8878279cf2bf
Chris Needham <chris.needham@bbc.co.uk>
2014-01-08 10:33:26 +0000

    Use winston for logging

--- app.js
+++ app.js
@@ -3,9 +3,4 @@
 var app = require('./lib/app');
 var http = require('http');
 
-
-http.createServer(app).listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-
+http.createServer(app).listen(app.get('port'));
