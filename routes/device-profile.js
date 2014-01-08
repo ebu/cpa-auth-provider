68e1be5354d71c8c0a92f5e37dbf97bc2f65d7cd
Chris Needham <chris.needham@bbc.co.uk>
2014-01-08 15:27:35 +0000

    Added requestAccessToken function

--- routes/device-profile.js
+++ routes/device-profile.js
@@ -5,56 +5,68 @@ var generate = require('../lib/generate');
 var verify = require('../lib/verify');
 var config = require('../config');
 
-module.exports = function (app, options) {
+function registerClient(req, res) {
+  var clientId = req.body.client_id;
 
+  if (!clientId) {
+    res.send(400);
+    return;
+  }
 
-// Client Registration Endpoint
-// http://tools.ietf.org/html/draft-ietf-oauth-dyn-reg-14#section-3
+  db.Client.find({ where: { id: clientId } }).success(function(client) {
+    if (client) {
 
-  app.post('/token', function(req, res) {
+      var newPairingProcess = {
+        device_code: generate.deviceCode(),
+        user_code: generate.userCode(),
+        verification_uri: config.uris.verification_uri
+      };
 
-    if (req.get('Content-Type') === 'application/x-www-form-urlencoded' &&
-        req.body.client_id &&
-        req.body.response_type === 'device_code') {
+      db.PairingProcess.create(newPairingProcess).success(function(pairingProcess) {
+        if (pairingProcess) {
+          pairingProcess.setClient(client);
 
-      var clientId = req.body.client_id;
-
-      db.Client.find({ where: { id: clientId } }).success(function(client) {
-        if (client) {
-
-          var newPairingProcess = {
-            device_code:  generate.deviceCode(),
-            user_code: generate.userCode(),
-            verification_uri: config.uris.verification_uri
-          };
-
-          db.PairingProcess.create(newPairingProcess).success(function(pairingProcess) {
-
-            if (pairingProcess) {
-              pairingProcess.setClient(client);
-
-              res.json(200, {
-                device_code: pairingProcess.device_code,
-                user_code: pairingProcess.user_code,
-                verification_uri: pairingProcess.verification_uri
-              });
-
-            } else {
-              res.send(500);
-            }
-
+          res.json(200, {
+            device_code: pairingProcess.device_code,
+            user_code: pairingProcess.user_code,
+            verification_uri: pairingProcess.verification_uri
           });
-
         } else {
-          res.send(400);
+          res.send(500);
         }
       });
-
     } else {
       res.send(400);
     }
-
   });
+}
 
+function requestAccessToken(req, res) {
+  res.send(400);
+}
+
+module.exports = function (app, options) {
+
+  // Client Registration Endpoint
+
+  app.post('/token', function(req, res) {
+    if (req.get('Content-Type') !== 'application/x-www-form-urlencoded') {
+      res.send(400);
+      return;
+    }
+
+    var responseType = req.body.response_type;
+
+    if (responseType === 'device_code') {
+      // http://tools.ietf.org/html/draft-ietf-oauth-dyn-reg-14#section-3
+      registerClient(req, res);
+    }
+    else if (responseType === 'code') {
+      // RFC6749 section 4.1.1
+      requestAccessToken(req, res);
+    }
+    else {
+      res.send(400);
+    }
+  });
 };
-
