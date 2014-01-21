1a23ccfe59fd2fe5ba0f5f6c64e0a8ceedf73651
Chris Needham <chris.needham@bbc.co.uk>
2014-01-21 15:55:19 +0000

    Use requestHelper.get

--- test/lib/home.js
+++ test/lib/home.js
@@ -3,21 +3,27 @@
 var requestHelper = require('../request-helper');
 
 describe('GET /', function() {
-  beforeEach(function(done) {
-    requestHelper.sendRequest(this, '/', done);
-  });
-
   context('with a signed in user', function() {
-    it('should redirect to /auth', function() {
+    before(function(done) {
+      requestHelper.get(this, '/', true, done);
+    });
+
+    it('should redirect to /verify', function() {
       expect(this.res.statusCode).to.equal(302);
-      // TODO: verify redirect
+      expect(this.res.headers).to.have.property('location');
+      // TODO: verify redirect location
     });
   });
 
   context('with no signed in user', function() {
-    it('should redirect to /verify', function() {
+    before(function(done) {
+      requestHelper.get(this, '/', false, done);
+    });
+
+    it('should redirect to /auth', function() {
       expect(this.res.statusCode).to.equal(302);
-      // TODO: verify redirect
+      expect(this.res.headers).to.have.property('location');
+      // TODO: verify redirect location
     });
   });
 });
