de1f7dd4b6077e6bcb430e9f42fa8c71c7665628
Chris Needham <chris.needham@bbc.co.uk>
2014-01-21 15:53:55 +0000

    Restored status tests

--- test/lib/status.js
+++ test/lib/status.js
@@ -2,22 +2,20 @@
 
 var requestHelper = require('../request-helper');
 
-describe('GET /', function() {
-  beforeEach(function(done) {
-    requestHelper.sendRequest(this, '/', done);
+describe('GET /status', function() {
+  before(function(done) {
+    requestHelper.sendRequest(this, '/status', done);
   });
 
-  context('with a signed in user', function() {
-    it('should redirect to /auth', function() {
-      expect(this.res.statusCode).to.equal(302);
-      // TODO: verify redirect
-    });
+  it('should return status 200', function() {
+    expect(this.res.statusCode).to.equal(200);
   });
 
-  context('with no signed in user', function() {
-    it('should redirect to /verify', function() {
-      expect(this.res.statusCode).to.equal(302);
-      // TODO: verify redirect
-    });
+  it('should respond with plain text', function() {
+    expect(this.res.headers["content-type"]).to.equal("text/plain; charset=utf-8");
+  });
+
+  it('should respond with status message', function() {
+    expect(this.res.text).to.equal("Authentication Provider up and running");
   });
 });
