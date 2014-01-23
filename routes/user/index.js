86deaaedecb5ab52e3dbc1c278baa74ea5437ae5
barroco <barroco@ebu.ch>
2014-01-23 11:55:39 +0100

    Add the list of tokens on /user/tokens

---
+++ routes/user/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var db = require('../../models');
+var config = require('../../config.js');
+var authHelper = require('../../lib/auth-helper');
+
+module.exports = function (app, options) {
+
+  app.get('/user/tokens',
+    authHelper.ensureAuthenticated,
+    function(req, res) {
+      db.ServiceAccessToken
+        .findAll()
+        .complete(function(err, tokens){
+        if(err) {
+          res.send(500);
+          return;
+        }
+        res.render('./user/token_list.ejs', {'tokens': tokens});
+      });
+
+    });
+
+};
+
