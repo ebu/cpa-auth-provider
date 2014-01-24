fd75e95cd1a5b4556e8bf48325658cec6f3231f5
Chris Needham <chris.needham@bbc.co.uk>
2014-01-24 07:58:32 +0000

    Use passport-local to test routes that require user to be signed in

--- test/lib/home.js
+++ test/lib/home.js
@@ -1,17 +1,57 @@
 "use strict";
 
+var db = require('../../models');
+
 var requestHelper = require('../request-helper');
 
+var resetDatabase = function(done) {
+  db.sequelize.query('DELETE FROM Users').then(function() {
+    return db.User.create({
+      provider_uid: 'testuser',
+      password: 'testpassword'
+    });
+  })
+  .then(function() {
+    done();
+  },
+  function(error) {
+    done(error);
+  });
+};
+
 describe('GET /', function() {
+  before(resetDatabase);
+
   context('with a signed in user', function() {
     before(function(done) {
-      requestHelper.get(this, '/', true, done);
+      var self = this;
+
+      request
+        .post('/login')
+        .type('form')
+        .send({ username: 'testuser', password: 'testpassword' })
+        .end(function(err, res) {
+          self.cookie = res.headers['set-cookie'];
+          done(err);
+        });
+    });
+
+    before(function(done) {
+      var self = this;
+
+      request
+        .get('/')
+        .set('cookie', this.cookie)
+        .end(function(err, res) {
+          self.res = res;
+          done(err);
+        });
     });
 
     it('should redirect to /verify', function() {
       expect(this.res.statusCode).to.equal(302);
       expect(this.res.headers).to.have.property('location');
-      // TODO: verify redirect location
+      expect(this.res.headers.location).to.equal('/verify');
     });
   });
 
@@ -23,7 +63,7 @@ describe('GET /', function() {
     it('should redirect to /auth', function() {
       expect(this.res.statusCode).to.equal(302);
       expect(this.res.headers).to.have.property('location');
-      // TODO: verify redirect location
+      expect(this.res.headers.location).to.equal('/auth');
     });
   });
 });
