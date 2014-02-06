df958bcd5c9b2cb0780bf3c3cf294db1fcc4dcd6
Chris Needham <chris.needham@bbc.co.uk>
2014-02-06 21:48:56 +0000

    Don't need to set NODE_ENV in test-helper.js

--- test/test-helper.js
+++ test/test-helper.js
@@ -7,8 +7,6 @@ global.sinon   = require("sinon");
 var sinonChai = require("sinon-chai");
 global.chai.use(sinonChai);
 
-process.env.NODE_ENV = 'test';
-
 var app = require('../lib/app');
 var supertest = require("supertest");
 global.request = supertest(app);
