4ca936091667ab48c97b0e1b3c959caf74bc7134
Chris Needham <chris.needham@bbc.co.uk>
2014-04-04 15:10:14 +0100

    Separated access token from user verification routes

--- routes/access-token.js
+++ routes/access-token.js
@@ -1,12 +1,8 @@
 "use strict";
 
-var config        = require('../config');
 var db            = require('../models');
-var authHelper    = require('../lib/auth-helper');
 var generate      = require('../lib/generate');
-var messages      = require('../lib/messages');
 var requestHelper = require('../lib/request-helper');
-var verify        = require('../lib/verify');
 
 var sendAccessToken = function(res, token, scope, user) {
   var name = (user !== null) ? user.provider_uid : "This radio";
@@ -169,55 +165,6 @@ var routes = function(app) {
       // Client mode
       requestClientModeAccessToken(res, clientId, clientSecret, scope);
     }
-  });
-
-  var renderVerificationPage = function(req, res, errorMessage) {
-    if (typeof errorMessage === 'string') {
-      res.status(400);
-      res.render('verify.ejs', { 'values': req.body, 'error': errorMessage });
-    }
-    else {
-      res.render('verify.ejs', { 'values': req.body, 'error': null });
-    }
-  };
-
-  app.get('/verify', authHelper.ensureAuthenticated, renderVerificationPage);
-
-  app.post('/verify', authHelper.ensureAuthenticated, function(req, res) {
-    if (!requestHelper.isContentType(req, 'application/x-www-form-urlencoded')) {
-      res.json(400, { error: 'invalid_request' });
-      return;
-    }
-
-    if (!req.body.user_code) {
-      logger.error("Missing user_code parameter");
-      res.json(400, { error: 'invalid_request' });
-      return;
-    }
-
-    var userCode = req.body.user_code;
-
-    verify.userCode(userCode, function(err, pairingCode) {
-      if (err || !pairingCode) {
-        renderVerificationPage(req, res, messages.INVALID_USERCODE);
-        return;
-      }
-
-      if (pairingCode.verified) {
-        res.status(400);
-        res.render('verify-info.ejs', { message: messages.OBSOLETE_USERCODE, status: 'warning' });
-        return;
-      }
-
-      pairingCode
-        .updateAttributes({ user_id: req.user.id, verified: true })
-        .success(function() {
-          res.render('verify-info.ejs', { message: messages.SUCCESSFUL_PAIRING, status: 'success' });
-        })
-        .error(function() {
-          res.send(500);
-        });
-    });
   });
 };
 
