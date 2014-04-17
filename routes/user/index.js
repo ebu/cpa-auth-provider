0286f6f2e1813c9f534ae4113cb1040f7646f74a
Chris Needham <chris.needham@bbc.co.uk>
2014-04-17 08:17:57 +0100

    Handle errors by passing Error object to next() function

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,23 +1,21 @@
 "use strict";
 
-var db = require('../../models');
-var config = require('../../config.js');
+var db         = require('../../models');
+var config     = require('../../config.js');
 var authHelper = require('../../lib/auth-helper');
 
 module.exports = function (app, options) {
+  app.get('/user/tokens', authHelper.ensureAuthenticated, function(req, res, next) {
+    db.ServiceAccessToken
+      .findAll({ include: [db.ServiceProvider] })
+      .complete(function(err, tokens) {
+        if (err) {
+          next(err);
+          return;
+        }
 
-  app.get('/user/tokens',
-    authHelper.ensureAuthenticated,
-    function(req, res) {
-      db.ServiceAccessToken
-        .findAll({include: [db.ServiceProvider]})
-        .complete(function(err, tokens){
-          if(err) {
-            res.send(500);
-            return;
-          }
-          res.render('./user/token_list.ejs', {'tokens': tokens});
-        });
+        res.render('./user/token_list.ejs', { tokens: tokens });
+      });
     });
 };
 
