72bed6e2d951fe0a59e3124d04edd6fb2a821e64
barroco <barroco@ebu.ch>
2014-12-11 12:13:41 +0100

    Remove deprecated service provider list endpoint

--- routes/user/index.js
+++ routes/user/index.js
@@ -4,18 +4,11 @@ var config     = require('../../config');
 var db         = require('../../models');
 var authHelper = require('../../lib/auth-helper');
 
-module.exports = function (app, options) {
-  app.get('/user/tokens', authHelper.ensureAuthenticated, function(req, res, next) {
-    db.ServiceAccessToken
-      .findAll({ include: [db.ServiceProvider] })
-      .complete(function(err, tokens) {
         if (err) {
           next(err);
           return;
         }
 
-        res.render('./user/token_list.ejs', { tokens: tokens });
       });
-    });
 };
 
