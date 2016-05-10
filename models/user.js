1f56c1596c919d8e69bd1f1b24dfc473d8730065
Arthur <arthur.anglade@rts.ch>
2016-05-10 09:47:35 +0200

    added salting to passwords in local database

--- models/user.js
+++ models/user.js
@@ -8,10 +8,26 @@ module.exports = function(sequelize, DataTypes) {
     password: DataTypes.STRING,
     enable_sso: DataTypes.BOOLEAN,
     display_name: DataTypes.STRING,
-    photo_url: DataTypes.STRING
+    photo_url: DataTypes.STRING,
+    admin: DataTypes.BOOLEAN   // maybe replace that by an array of roles
   }, {
     underscored: true,
-
+    instanceMethods: {
+      setPassword: function(password, done) {
+        return bcrypt.genSalt(10, function(err, salt) {
+          return bcrypt.hash(password, salt, function(error, encrypted) {
+            this.password = encrypted;
+            this.salt = salt;
+            return done();
+          });
+        });
+      },
+      verifyPassword: function(password, done) {
+        return bcrypt.compare(password, this.password, function(err, res) {
+          return done(err, res);
+        });
+      }
+    },
     associate: function(models) {
       User.hasMany(models.Client);
       User.hasMany(models.AccessToken);
