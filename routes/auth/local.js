cac0ce09243365069869c1a73d523af327e8ca8f
Arthur <arthur.anglade@rts.ch>
2016-09-22 14:54:10 +0200

    error messages displayed correctly

--- routes/auth/local.js
+++ routes/auth/local.js
@@ -10,10 +10,11 @@ var LocalStrategy = require('passport-local').Strategy;
 var recaptcha     = require('express-recaptcha');
 
 var localStrategyCallback = function(req, username, password, done) {
+
   db.User.find({ where: { email: username} })
     .then(function(user) {
       if (!user) {
-        done('user not found');
+        done(null, false, req.flash('loginMessage', 'No user found.'));
         return;
       }
 
@@ -21,7 +22,7 @@ var localStrategyCallback = function(req, username, password, done) {
         if (isMatch) {
           done(null, user);
         } else {
-          done('password does not match');
+          done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
         }
       },
       function(err) {
@@ -33,24 +34,58 @@ var localStrategyCallback = function(req, username, password, done) {
     });
 };
 
+var localSignupStrategyCallback = function(req, username, password, done) {
+
+    if (req.recaptcha.error) {
+        done(null, false, req.flash('signupMessage', 'Something went wrong with the reCAPTCHA'));
+        return;
+    }
+
+    db.User.find({ where: { email: req.body.email} })
+        .then (function (user){
+            if (user){
+                done(null, false, req.flash('signupMessage', 'That email is already taken'));
+            } else {
+                db.sequelize.sync().then(function() {
+                    var user = db.User.create({
+                        email: req.body.email,
+                    }).then(function (user) {
+                        user.setPassword(req.body.password);
+                        done(null, user);
+                    },
+                    function(err){
+                        done(err);
+                    });
+                });
+        }
+    }, function(error) {
+            done(error);
+        });
+
+};
+
+var localStrategyConf = {
+    // by default, local strategy uses username and password, we will override with email
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true // allows us to pass back the entire request to the callback
+};
+
 // Google reCAPTCHA
 recaptcha.init(config.recaptcha.site_key, config.recaptcha.secret_key);
 
-passport.use('local',new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },localStrategyCallback));
+passport.use('local',new LocalStrategy(localStrategyConf,localStrategyCallback));
+
+passport.use('local-signup', new LocalStrategy(localStrategyConf, localSignupStrategyCallback));
 
 module.exports = function(app, options) {
 
   app.get('/auth/local', function(req, res) {
-      res.render('login.ejs');
+      res.render('login.ejs', {message: req.flash('loginMessage')});
   });
 
   app.get('/signup', function(req, res) {
-      res.render('signup.ejs', {email: req.query.email});
+      res.render('signup.ejs', {email: req.query.email, message: req.flash('signupMessage')});
   });
 
   app.get('/logout', function(req, res) {
@@ -60,7 +95,8 @@ module.exports = function(app, options) {
 
 
   app.post('/login', passport.authenticate('local', {
-    failureRedirect: '/auth/local?error=login_failed'
+    failureRedirect: '/auth/local',
+    failureFlash: true
   }), function (req, res, next) {
     var redirectUri = req.session.auth_origin;
     delete req.session.auth_origin;
@@ -71,36 +107,10 @@ module.exports = function(app, options) {
 
     requestHelper.redirect(res, '/');
   });
-  
-      // process the signup form
-  app.post('/signup', recaptcha.middleware.verify , function(req, res) {
 
-    if (req.recaptcha.error) {
-      requestHelper.redirect(res, '/signup?error='+ req.recaptcha.error+'&email='+req.body.email);
-      return;
-    }
-
-    if (req.body.password != req.body.password2){
-      requestHelper.redirect(res, '/signup?error=passwords_dont_match&email='+req.body.email);
-      return;
-    } 
-
-    db.User.find({ where: { email: req.body.email} }).then (function (user){
-      if (user){
-        console.log('login found : ' + user.get('email'));
-        requestHelper.redirect(res, '/signup?error=login_already_exists&email='+req.body.email);
-      } else {
-        db.sequelize.sync().then(function() {
-        var user = db.User.create({
-            email: req.body.email,
-          }).then(function (user) {
-            return user.setPassword(req.body.password);
-          });
-        })
-        requestHelper.redirect(res, '/auth/local');
-      }
-    });
-    
-
-  });
+  app.post('/signup', recaptcha.middleware.verify , passport.authenticate('local-signup', {
+      failureRedirect: '/signup',
+      successRedirect: '/auth/local',
+      failureFlash : true
+  }));
 };
