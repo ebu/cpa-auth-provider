62489367808e6f912447b10bac537c97ba231d3a
Daniel Krax <daniel.krax@br.de>
2016-12-14 17:16:03 +0100

    set FROM by environment variable

--- config.docker.js
+++ config.docker.js
@@ -85,6 +85,8 @@ module.exports = {
     },
   ],
 
+  email_from: process.env.EMAIL_FROM,
+
   oauth2: {
     access_token_duration: 10 * 60 * 60 * 1000,
     refresh_token_duration: 365 * 24 * 60 * 60 * 1000,
