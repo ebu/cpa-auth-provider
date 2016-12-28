93f9a05f170abdb2efbc1cfb36db125434a77b13
dom <dominique.chion@rts.ch>
2016-12-28 15:23:12 +0100

    End of test of user_profile.getDisplayName

--- test/lib/user-profile.js
+++ test/lib/user-profile.js
@@ -8,13 +8,62 @@ describe('Test user profile', function () {
 
             it('should save without error', function (done) {
 
-                var user =  db.User.build({
+                var user = db.User.build({
                     id: 1,
                     email: 'user1@earth.com',
                     provider_uid: 'testuser',
                     display_name: 'Test User 1'
                 });
-                var user_profile =  db.UserProfile.build({
+                var user_profile = db.UserProfile.build({
+                    id: 1,
+                    user_id: 1
+                });
+                
+                expect(user_profile.getDisplayName(user, 'FIRSTNAME')).to.equal('user1@earth.com');
+                expect(user_profile.getDisplayName(user, 'LASTNAME')).to.equal('user1@earth.com');
+                expect(user_profile.getDisplayName(user, 'FIRSTNAME_LASTNAME')).to.equal('user1@earth.com');
+                expect(user_profile.getDisplayName(user, 'EMAIL')).to.equal('user1@earth.com');
+
+                user_profile = db.UserProfile.build({
+                    id: 1,
+                    lastname: 'lastname',
+                    gender: 'gender',
+                    birthdate: 'birthdate',
+                    user_id: 1
+                });
+
+                expect(user_profile.getDisplayName(user, 'FIRSTNAME')).to.equal('user1@earth.com');
+
+                user_profile = db.UserProfile.build({
+                    id: 1,
+                    firstname: 'firstname',
+                    gender: 'gender',
+                    birthdate: 'birthdate',
+                    user_id: 1
+                });
+
+                expect(user_profile.getDisplayName(user, 'LASTNAME')).to.equal('user1@earth.com');
+                user_profile = db.UserProfile.build({
+                    id: 1,
+                    lastname: 'lastname',
+                    gender: 'gender',
+                    birthdate: 'birthdate',
+                    user_id: 1
+                });
+
+                expect(user_profile.getDisplayName(user, 'FIRSTNAME_LASTNAME')).to.equal('user1@earth.com');
+
+                user_profile = db.UserProfile.build({
+                    id: 1,
+                    firstname: 'firstname',
+                    gender: 'gender',
+                    birthdate: 'birthdate',
+                    user_id: 1
+                });
+
+                expect(user_profile.getDisplayName(user, 'FIRSTNAME_LASTNAME')).to.equal('user1@earth.com');
+
+                user_profile = db.UserProfile.build({
                     id: 1,
                     firstname: 'firstname',
                     lastname: 'lastname',
@@ -22,7 +71,12 @@ describe('Test user profile', function () {
                     birthdate: 'birthdate',
                     user_id: 1
                 });
-                expect(user_profile.getDisplayName(user, 'FIRST_NAME')).to.equal('user1@earth.com');
+
+                expect(user_profile.getDisplayName(user, 'FIRSTNAME')).to.equal('firstname');
+                expect(user_profile.getDisplayName(user, 'LASTNAME')).to.equal('lastname');
+                expect(user_profile.getDisplayName(user, 'FIRSTNAME_LASTNAME')).to.equal('firstname lastname');
+                expect(user_profile.getDisplayName(user, 'EMAIL')).to.equal('user1@earth.com');
+
 
                 done();
             });
