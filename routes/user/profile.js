10d02253b2746cb7bcae3fb769252a37d9ff3529
Alexandre CAPELLARO <alexandre.capellaro@hortis.ch>
2016-12-30 08:58:44 +0100

    WIP validators

--- routes/user/profile.js
+++ routes/user/profile.js
@@ -3,32 +3,41 @@
 var config     = require('../../config');
 var db         = require('../../models/index');
 var authHelper = require('../../lib/auth-helper');
-var async      = require('async');
+var util       = require('util');
 
-var routes = function(router) {
-  router.put('/user/profile/:user_id', authHelper.ensureAuthenticated, function(req, res) {
-    var userId = req.params.user_id;
+var routes = function (router) {
+    router.put('/user/profile/:user_id', authHelper.ensureAuthenticated, function (req, res) {
+        var userId = req.params.user_id;
+        req.checkBody('firstname', 'Champ nom non valide').notEmpty().isInt();
 
-    db.UserProfile.findOrCreate({
-      user_id: userId
-    }).then(function (user_profile) {
-          user_profile.updateAttributes(
-              {
-                firstname: req.body.firstname ? req.body.firstname : user_profile.firstname,
-                lastname: req.body.lastname ? req.body.lastname : user_profile.lastname,
-                gender: req.body.gender ? req.body.gender : user_profile.gender,
-                birthdate: req.body.birthdate ? req.body.birthdate : user_profile.birthdate
-              })
-              .then(function () {
-                    res.json({msg: 'Successfully updated user_profile.'});
-                  },
-                  function (err) {
-                    res.status(500).json({msg: 'Cannot update user_profile. Err:' + err});
-                  });
-        }
-    );
+        req.getValidationResult().then(function(result) {
+            if (!result.isEmpty()) {
+                res.status(400).send('There have been validation errors: ' + util.inspect(result.array()));
+                return;
+            }
+            res.json({msg: 'All good'});
+        });
 
-  });
+        // db.UserProfile.findOrCreate({
+        //     user_id: userId
+        // }).then(function (user_profile) {
+        //         user_profile.updateAttributes(
+        //             {
+        //                 firstname: req.body.firstname,
+        //                 lastname: req.body.lastname,
+        //                 gender: req.body.gender,
+        //                 birthdate: req.body.birthdate ? req.body.birthdate : user_profile.birthdate
+        //             })
+        //             .then(function () {
+        //                     res.json({msg: 'Successfully updated user_profile.'});
+        //                 },
+        //                 function (err) {
+        //                     res.status(500).json({msg: 'Cannot update user_profile. Err:' + err});
+        //                 });
+        //     }
+        // );
+
+    });
 };
 
 module.exports = routes;
