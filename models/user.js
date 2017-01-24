817ddb77fdee74ee95a784c578985d6667d24206
dom <dominique.chion@rts.ch>
2017-01-24 09:18:13 +0100

    Code format

--- models/user.js
+++ models/user.js
@@ -3,44 +3,44 @@
 var Promise = require('bluebird');
 var bcrypt = Promise.promisifyAll(require('bcrypt'));
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
 
-  var User = sequelize.define('User', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    tracking_uid: DataTypes.STRING,
-    provider_uid: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    enable_sso: DataTypes.BOOLEAN,
-    display_name: DataTypes.STRING,
-    photo_url: DataTypes.STRING,
-    admin: DataTypes.BOOLEAN   // maybe replace that by an array of roles
-  }, {
-    underscored: true,
-    instanceMethods: {
-      setPassword: function(password) {
-        var self = this;
-        return bcrypt.genSaltAsync(10).then(function(salt) {
-          return bcrypt.hashAsync(password, salt);
-        })
-        .then(function(encrypted, salt) {
-          return self.updateAttributes({ password: encrypted });
-        });
-      },
-      verifyPassword: function(password) {
-        return bcrypt.compareAsync(password, this.password);
-      },
-      hasChanged: function (displayName, photoUrl) {
-        return (this.display_name !== displayName || this.photo_url !== photoUrl);
-      }
-    },
-    associate: function(models) {
-      User.hasMany(models.Client);
-      User.hasMany(models.AccessToken);
-      User.belongsTo(models.IdentityProvider);
-      User.hasOne(models.UserProfile);
-    }
-  });
+    var User = sequelize.define('User', {
+        id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+        tracking_uid: DataTypes.STRING,
+        provider_uid: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        enable_sso: DataTypes.BOOLEAN,
+        display_name: DataTypes.STRING,
+        photo_url: DataTypes.STRING,
+        admin: DataTypes.BOOLEAN   // maybe replace that by an array of roles
+    }, {
+        underscored: true,
+        instanceMethods: {
+            setPassword: function (password) {
+                var self = this;
+                return bcrypt.genSaltAsync(10).then(function (salt) {
+                        return bcrypt.hashAsync(password, salt);
+                    })
+                    .then(function (encrypted, salt) {
+                        return self.updateAttributes({password: encrypted});
+                    });
+            },
+            verifyPassword: function (password) {
+                return bcrypt.compareAsync(password, this.password);
+            },
+            hasChanged: function (displayName, photoUrl) {
+                return (this.display_name !== displayName || this.photo_url !== photoUrl);
+            }
+        },
+        associate: function (models) {
+            User.hasMany(models.Client);
+            User.hasMany(models.AccessToken);
+            User.belongsTo(models.IdentityProvider);
+            User.hasOne(models.UserProfile);
+        }
+    });
 
-  return User;
+    return User;
 };
