b5073bfa88494f1b49d09e1be729df0b85e15cdd
dom <dominique.chion@rts.ch>
2017-01-24 11:02:54 +0100

    Clean test

--- test/lib/user-register.js
+++ test/lib/user-register.js
@@ -4,22 +4,21 @@ var db = require('../../models');
 
 describe('Test verification code', function () {
     context('When validating user account', function () {
-            var user;
             before (function (){
-                user = db.User.build({
+                this.user = db.User.build({
                     id: 1,
                     email: 'user1@earth.com',
                     provider_uid: 'testuser',
                     display_name: 'Test User 1'
                 });
-                user.genereateVerificationCode();
+                this.user.genereateVerificationCode();
             })
         it('should return false when validation code is wrong', function (done) {
-            expect(user.verifyAccount('this is a wrong code')).to.be.false;
+            expect(this.user.verifyAccount('this is a wrong code')).to.be.false;
             done();
         });
         it('should return true when validation code is correct', function (done) {
-            expect(user.verifyAccount(user.verificationCode)).to.be.true;
+            expect(this.user.verifyAccount(this.user.verificationCode)).to.be.true;
             done();
         });
     })
