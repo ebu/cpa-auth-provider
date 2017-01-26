f46731ca1a6d4a19e6c14e754f47b07e923eb4f2
dom <dominique.chion@rts.ch>
2017-01-26 16:54:28 +0100

    Test WIP

---
+++ test/lib/password-recovery.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var db = require('../../models');
+
+describe('Test password recovery code', function () {
+    context('When tries to recover password', function () {
+        before(function () {
+            this.user = db.User.build({
+                id: 1,
+                email: 'user1@earth.com',
+                provider_uid: 'testuser',
+                display_name: 'Test User 1'
+            });
+            this.user.setPassword('mdp');
+            this.user.generateRecoveryCode();
+            this.currentPass = this.user.password;
+        })
+        it('should do nothing when code is empty', function (done) {
+            this.user.recoverPassword('', 'new pass');
+            expect(this.user.password).to.be.equal(this.currentPass);
+            done();
+        });
+        it('should do nothing when code is wrong', function (done) {
+            this.user.recoverPassword('wrong code', 'new pass');
+            expect(this.user.password).to.be.equal(this.currentPass);
+            done();
+        });
+        it('should  udpate the password and remove recovery code stuff', function (done) {
+            this.user.recoverPassword(this.user.passwordRecoveryCode, 'new pass');
+            expect(this.user.password).not.to.be.equal(this.currentPass);
+            expect(this.user.passwordRecoveryCode).to.be.empty();
+            expect(this.user.passwordRecoveryCodeDate).to.be.equal(0);
+            done();
+        });
+    })
+});
