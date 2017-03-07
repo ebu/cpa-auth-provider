2b5afd5ae88c9f75eae6238e6ae103c48db26064
dom <dominique.chion@rts.ch>
2017-03-07 11:41:14 +0100

    Doc WIP and config comment/formating/refactoring

--- config.dist.js
+++ config.dist.js
@@ -2,181 +2,198 @@
 
 module.exports = {
 
-  i18n: {
-      cookie_name:'language',
-      cookie_duration: 365 * 24 * 60 * 60 * 1000,
-      default_locale: 'en'
-  },
-
-
-  jwtSecret:'bigsecret',
-  jwt: {
-    audience: 'cpa',
-    issuer: 'cpa'
-  },
-
-  identity_providers: {
-    // Details : http://passportjs.org/guide/facebook/
-    facebook: {
-      enabled: false,
-      client_id: '',
-      client_secret: '',
-      callback_url: ''
+    // Define the available identity providers
+    identity_providers: {
+        // Details : http://passportjs.org/guide/facebook/
+        facebook: {
+            enabled: false,
+            client_id: '',
+            client_secret: '',
+            callback_url: ''
+        },
+        github: {
+            enabled: false,
+            client_id: '',
+            client_secret: '',
+            callback_url: ''
+        },
+        ebu: {
+            enabled: false
+        },
+        local: {
+            enabled: true
+        }
     },
-    github: {
-      enabled: false,
-      client_id: '',
-      client_secret: '',
-      callback_url: ''
+
+    // configuration for password using local identity provider
+    password: {
+        // in sec
+        recovery_code_validity_duration: 1800,
+        // a new recovery code will be generated only if the current one has less that TTL
+        keep_recovery_code_until: 900
     },
-    ebu: {
-      enabled: false
+
+    // define the name of the cookie used to store user locale
+    i18n: {
+        cookie_name: 'language',
+        cookie_duration: 365 * 24 * 60 * 60 * 1000,
+        default_locale: 'en'
     },
-    local: {
-      enabled: true
-    }
-  },
 
-  mail: {
-    sending: {
-      // transport: 'sendmail'
-      // transport: 'stream'
-      transport: 'gmail',
-      username: 'ebuidp@gmail.com',
-      password: 'HalloEBU',
+    // mail configuration, available configuration are 'test','direct','sendmail','smtp','aws', 'gmail'
+    mail: {
+        sending: {
+            transport: 'test',
+            // transport: 'direct'
+            // transport: 'sendmail'
+            // transport: 'smtp'
+            // transport: 'aws'
+            // transport: 'gmail',
+            username: 'username',
+            password: 'password',
+        },
+        from: 'no-reply@broadcaster.com',
+        // host is used for to generate link in mail (like password recovery)
+        host: 'http://localhost:3000'
     },
-    from:'no-reply@rts.ch',
-    host:'http://localhost:3000'
-  },
 
-  password: {
-    // in sec
-    recovery_code_validity_duration: 1800,
-    // a new recovery code will be generated only if the current one has less that TTL
-    keep_recovery_code_until: 900
-  },
-
-  trackingCookie: {
-    enabled: false,
-    secret: 'secret phrase',
-    // name: 'TrackingId',
-    duration: 10 * 365 * 24 * 60 * 60 * 1000 // 10 years
-  },
-
-  // recaptcha test keys
-  recaptcha: {
-    site_key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
-    secret_key: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
-  },
-
-  // When accessing the home page, if defined, users are automatically
-  // redirected to the specified identity_providers (ie: 'github')
-  auto_idp_redirect: '',
-
-  displayUsersInfos: true,
-
-  db: {
-    host: '',
-    port: 3306,
-    user: '',
-    password: '',
-
-    // The database type, 'mysql', 'sqlite', etc.
-    type: 'sqlite',
-    database: '',
-
-    // Database filename for SQLite.
-    filename: 'db.db',
-
-    // If true, SQL statements are logged to the console.
-    debug: true
-  },
-
-  // Session cookie is signed with this secret to prevent tampering
-  session_secret: 'LKASDMjnr234n90lasndfsadf',
-
-  // Name of the session cookie. Must be something different than 'connect.sid'
-  sid_cookie_name: 'identity.provider.sid',
-
-  // Cross-origin resource sharing
-  cors: {
-    enabled: true,
-    allowed_domains: [
-      "http://cpa-client.ebu.io"
-    ]
-  },
-
-  // URL path prefix, e.g., '/myapp'
-  urlPrefix: '',
-
-  // Broadcaster specific layout
-  broadcasterLayout: '',
-
-  // The end-user verification URI on the authorization server. The URI should
-  // be short and easy to remember as end-users will be asked to manually type
-  // it into their user-agent.
-  verification_uri: 'http://vagrant.ebu.io:3000/',
-
-  domains: [
-    {
-      name:         'bbc1-cpa.ebu.io',
-      display_name: 'BBC1',
-      access_token: 'b4949eba147f4cf88985b43c039cd05b'
+    trackingCookie: {
+        enabled: false,
+        secret: 'secret phrase',
+        duration: 10 * 365 * 24 * 60 * 60 * 1000 // 10 years
     },
-    {
-      name:         'bbc2-cpa.ebu.io',
-      display_name: 'BBC2',
-      access_token: 'b3dd734356524ef9b9ab3d03f1b1558e'
-    }
-  ],
 
-  permissions: [
-    {
-      id:       1,
-      label:    "admin"
+    // google recaptcha keys
+    recaptcha: {
+        // test keys
+        // site_key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+        // secret_key: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+        site_key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+        secret_key: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
     },
-    {
-      id:       2,
-      label:    "other"
-    }
-  ],
 
-  // users: [
-  //   {
-  //     id:           1,
-  //     email:        "admin@admin.com",
-  //     display_name: "Admin",
-  //     verified:     true,
-  //     permission_id:1
-  //   }
-  // ],
+    // When accessing the home page, if defined, users are automatically
+    // redirected to the specified identity_providers (ie: 'github')
+    auto_idp_redirect: '',
 
-  // This option controls how the authorization server responds to requests to
-  // associate an existing client with a new domain:
-  // - false: The user must authenticate and confirm access to the new domain
-  // - true: The user is automatically granted access without confirmation
-  auto_provision_tokens: false,
+    // if false the list of user is not accessible in the admin
+    displayUsersInfos: true,
 
-  server_clients : [{
-    name: '',
-    software_id: '',
-    software_version: '',
-    ip: '127.0.0.1',
-    secret: '',
-    registration_type: 'static',
-    redirect_uri: ''
-  }],
+    // Database configuration
+    db: {
+        host: '',
+        port: 3306,
+        user: '',
+        password: '',
 
-  // The length of time that user (pairing) codes are valid, in seconds.
-  pairing_code_lifetime: 60 * 60, // 1 hour
+        // The database type, 'mysql', 'sqlite', etc.
+        type: 'sqlite',
+        database: '',
 
-  // The length of time that a access tokens are valid, in seconds.
-  access_token_lifetime: 24 * 60 * 60, // 1 day
+        // Database filename for SQLite.
+        filename: 'db.db',
 
-  // The length of time that an authorization code is valid, in seconds.
-  authorization_code_lifetime: 10 * 60, // 10 minutes
+        // If true, SQL statements are logged to the console.
+        debug: true
+    },
 
-  // The maximum rate at which clients should poll to obtain an access token,
-  // in seconds.
-  max_poll_interval: 5,
+    // Session cookie is signed with this secret to prevent tampering
+    session_secret: 'LKASDMjnr234n90lasndfsadf',
+
+    // Name of the session cookie. Must be something different than 'connect.sid'
+    sid_cookie_name: 'identity.provider.sid',
+
+    // Cross-origin resource sharing
+    cors: {
+        enabled: true,
+        allowed_domains: [
+            "http://cpa-client.ebu.io"
+        ]
+    },
+
+    // URL path prefix, e.g., '/myapp'
+    urlPrefix: '',
+
+    // Name of the Broadcaster specific layout. Use '' for default one.
+    broadcasterLayout: '',
+
+    // CPA
+
+    // The length of time that user (pairing) codes are valid, in seconds.
+    pairing_code_lifetime: 60 * 60, // 1 hour
+
+    // The length of time that a access tokens are valid, in seconds.
+    access_token_lifetime: 24 * 60 * 60, // 1 day
+
+    // The length of time that an authorization code is valid, in seconds.
+    authorization_code_lifetime: 10 * 60, // 10 minutes
+
+    // The maximum rate at which clients should poll to obtain an access token,
+    // in seconds.
+    max_poll_interval: 5,
+
+    // JWT configuration for CPA https://tech.ebu.ch/groups/CPA
+    jwtSecret: 'CHANGE_BEFORE_MOVING_IN_PRODUCTION',
+    jwt: {
+        audience: 'cpa',
+        issuer: 'cpa'
+    },
+
+    // The end-user verification URI on the authorization server. The URI should
+    // be short and easy to remember as end-users will be asked to manually type
+    // it into their user-agent.
+    verification_uri: 'http://vagrant.ebu.io:3000/',
+
+
+    // This option controls how the authorization server responds to requests to
+    // associate an existing client with a new domain:
+    // - false: The user must authenticate and confirm access to the new domain
+    // - true: The user is automatically granted access without confirmation
+    auto_provision_tokens: false,
+
+    server_clients: [{
+        name: '',
+        software_id: '',
+        software_version: '',
+        ip: '127.0.0.1',
+        secret: '',
+        registration_type: 'static',
+        redirect_uri: ''
+    }],
+
+    // Data that will be inserted when running bin/initdb
+    domains: [
+        {
+            name: 'bbc1-cpa.ebu.io',
+            display_name: 'BBC1',
+            access_token: 'b4949eba147f4cf88985b43c039cd05b'
+        },
+        {
+            name: 'bbc2-cpa.ebu.io',
+            display_name: 'BBC2',
+            access_token: 'b3dd734356524ef9b9ab3d03f1b1558e'
+        }
+    ],
+
+    permissions: [
+        {
+            id: 1,
+            label: "admin"
+        },
+        {
+            id: 2,
+            label: "other"
+        }
+    ],
+
+    // users: [
+    //   {
+    //     id:           1,
+    //     email:        "admin@admin.com",
+    //     display_name: "Admin",
+    //     verified:     true,
+    //     permission_id:1
+    //   }
+    // ],
+
 };
--- doc/getting-started.md
+++ doc/getting-started.md
@@ -9,34 +9,86 @@ This software implements version 1.0 of the Cross-Platform Authentication Protoc
 
 More information on the [EBU Cross-Platform Authentication project](http://tech.ebu.ch/cpa).
 
-## Prerequisites
+##Getting (quick) started using docker
+
+First run [docker](https://www.docker.com/) on you machine.
+Then execute the following commands:
+
+```
+$ git clone https://git.ebu.io/pipe/cpa-auth-provider.git
+$ cd cpa-auth-provide
+$ docker-compose up
+```
+
+Now you have 2 sample web servers on your machine.
+You can reach those as [http://localhost:3000](http://localhost:3000) and [http://localhost:3001](http://localhost:3001).
+
+[http://localhost:3000](http://localhost:3000) is the identity service.
+
+[http://localhost:3001](http://localhost:3001) demonstrates [the OAuth 2.0 implementation](#demo-site-for-oauth-20-implementation). It will redirect and use the identity service.
+
+
+##Starting the identity provider outside of docker
 
 Ensure your system has [Node.js](http://nodejs.org/) (v0.10 or later) and NPM installed.
 
-## Getting started
-
+```
 $ git clone https://git.ebu.io/pipe/cpa-auth-provider.git
 $ cd cpa-auth-provider
 $ npm install
 $ cp config.dist.js config.local.js
 $ NODE_ENV=development bin/init-db
-
-
-
-
-## Getting started
-After cloning the repository, you can use
-docker-compose to start up the sample.
-```
-docker-compose up
+$ bin/server
 ```
 
-It will run sample web servers on your machine.
-You can reach those as `http://localhost:3000`
-and `http://localhost:3001`.
+Now you have an identity provider listening on http://localhost:3000/
 
-`http://localhost:3000` is the identity service.
+##Starting the sample site outside of docker
 
-`http://localhost:3001` demonstrates the OAuth 2.0
-implementation. It will redirect and use the
-identity service.
+Using a bash shell
+
+```
+$ cd cpa-auth-provider
+$ export DEMO_HOST=http://localhost
+$ export OAUTH2_CLIENT_ID=db05acb0c6ed902e5a5b7f5ab79e7144
+$ export OAUTH2_CLIENT_SECRET=49b7448061fed2319168eb2449ef3b58226a9c554b3ff0b138abe8ffad98
+$ export OAUTH2_SERVER=\$DEMO_HOST:3000
+$ export OAUTH2_CALLBACK=\$DEMO_HOST:3001
+$ export OAUTH2_INTERNAL_SERVER=\$DEMO_HOST:3000
+$ export HTTP_PORT=3001
+$ node oauth2-client/app.js
+```
+
+Now you have a [demo site for oAuth 2.0](#demo-site-for-oauth-20-implementation) listening on http://localhost:3001/
+
+
+## Configuration
+
+The file `config.dist.js` contains a sample configuration. **This configuration is not supposed to be used as is in production.**
+
+| File | Description |
+| ----------------- | ----------- |
+| config.local.js   | Configuration used when [running the identity provider outside docker](#starting-the-identitiy-provider-outside-of-docker)  |
+| config.docker.js  | Configuration used when [running the identity provider inside docker](#getting-quick-started-using-docker) |
+| config.test.js    | Configuration used for unit tests |
+
+
+## Customise layout
+
+You can customize css, header and footer.
+
+- Declare your layout name in your [config file](#configuration) in property `broadcasterLayout` (sample: ebu)
+- Create a folder in `views/layout` for your custom files (sample: `views/layout/ebu')
+- Add your css file in `public/css" (sample ebu.css) \[Optional\]
+- Add your custom files in the created folder: 
+	- `views/layout/ebu/footer.ejs': custom footer
+	- `views/layout/ebu/header.ejs': custom header (include a reference to your custom css in that file)
+	- `views/layout/ebu/nav.ejs': custom navigation
+- Update `views/layout/head.ejs` and `views/layout/foot.ejs' to include previous files
+
+
+##Demo site for OAuth 2.0 implementation
+
+
+
+
