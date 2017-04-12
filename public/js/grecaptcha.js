e7f203cc42331cf3e8a8268f89901737ee95d0ba
Alexandre CAPELLARO <alexandre.capellaro@hortis.ch>
2017-04-12 15:47:41 +0200

    FIX recracha error on pages that does not include any

--- public/js/grecaptcha.js
+++ public/js/grecaptcha.js
@@ -1,11 +1,13 @@
 "use strict";
 
 function recaptchaReady () {
-    grecaptcha.render('myrecaptcha', {
-        'sitekey': siteKey,
-        'expired-callback': function () {
-            grecaptcha.reset();
-            console.log('recatpcha');
-        }
-    });
+    if($('#myrecaptcha').length) {
+        grecaptcha.render('myrecaptcha', {
+            'sitekey': siteKey,
+            'expired-callback': function () {
+                grecaptcha.reset();
+                console.log('recatpcha');
+            }
+        });
+    }
 }
