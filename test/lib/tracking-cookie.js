a09051779412a1236c104f75dcc6afeb616fcc56
Alexandre CAPELLARO <alexandre.capellaro@hortis.ch>
2017-05-01 16:12:43 +0200

    Cookie tracking enabled is now a docker env parameter

--- test/lib/tracking-cookie.js
+++ test/lib/tracking-cookie.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var config = require('../../config');
 var requestHelper = require("../request-helper");
 
 describe('GET /auth - for tracking cookie', function () {
@@ -10,5 +11,17 @@ describe('GET /auth - for tracking cookie', function () {
     it('should set a cookie', function () {
         expect(this.res.header['set-cookie']).to.be.an('array');
         expect(this.res.header['set-cookie'].length).to.equal(2);
+    });
+});
+
+describe('GET /auth - for tracking cookie', function () {
+    before(function (done) {
+        config.trackingCookie.enabled = false;
+        requestHelper.sendRequest(this, '/auth', null, done);
+    });
+
+    it('should not set a cookie if config.trackingCookie.enabled is false', function () {
+        expect(this.res.header['set-cookie']).to.be.an('array');
+        expect(this.res.header['set-cookie'].length).to.equal(1);
     });
 });
