45aaa26835769b6419559a53f72ad06c0a2f2a14
Alexandre CAPELLARO <alexandre.capellaro@hortis.ch>
2017-05-02 15:09:42 +0200

    ignoreme parameter for /auth curl access

--- test/lib/tracking-cookie.js
+++ test/lib/tracking-cookie.js
@@ -1,7 +1,18 @@
 "use strict";
 
 var config = require('../../config');
+
+var generate = require('../../lib/generate');
+var db = require('../../models');
+
+var dbHelper = require('../db-helper');
 var requestHelper = require("../request-helper");
+
+var resetDatabase = function (done) {
+    dbHelper.clearDatabase(function (err) {
+        done(err);
+    });
+};
 
 describe('GET /auth - for tracking cookie', function () {
     before(function (done) {
@@ -23,5 +34,55 @@ describe('GET /auth - for tracking cookie', function () {
     it('should not set a cookie if config.trackingCookie.enabled is false', function () {
         expect(this.res.header['set-cookie']).to.be.an('array');
         expect(this.res.header['set-cookie'].length).to.equal(1);
+    });
+});
+
+describe('GET /auth - for ignore parameter', function () {
+
+    var nbUsers = 0;
+
+    before(resetDatabase);
+
+    before(function (done) {
+        config.trackingCookie.enabled = true;
+        requestHelper.sendRequest(this, '/auth', null, done);
+    });
+
+    before(function (done) {
+        db.User.count().then(
+            function (users) {
+                nbUsers = users;
+                done();
+            },
+            done);
+    });
+
+    it('should add a new user', function () {
+        expect(nbUsers).to.equal(1);
+    });
+});
+
+describe('GET /auth - for ignore parameter', function () {
+
+    var nbUsers = 0;
+
+    before(resetDatabase);
+
+    before(function (done) {
+        config.trackingCookie.enabled = true;
+        requestHelper.sendRequest(this, '/auth?ignoreme=true', null, done);
+    });
+
+    before(function (done) {
+        db.User.count().then(
+            function (users) {
+                nbUsers = users;
+                done();
+            },
+        done);
+    });
+
+    it('should not add a new user', function () {
+        expect(nbUsers).to.equal(0);
     });
 });
