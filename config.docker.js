1995887c6e546fe512d2a5870f458f5ac272b241
dom <dominique.chion@rts.ch>
2017-05-11 14:54:37 +0200

    Parameter for auto_idp_redirect

--- config.docker.js
+++ config.docker.js
@@ -95,7 +95,7 @@ module.exports = {
 
     // When accessing the home page, if defined, users are automatically
     // redirected to the specified identity_providers (ie: 'github')
-    auto_idp_redirect: '',
+    auto_idp_redirect: process.env.AUTO_IDP_REDIRECT || '',
 
     db: {
         host: process.env.DB_HOST,
