63cea10a3fdc4ec2b0010d79d203c1269d45afb9
dom <dominique.chion@rts.ch>
2017-05-12 11:34:07 +0200

    support for logout

--- routes/auth/oauth-local.js
+++ routes/auth/oauth-local.js
@@ -90,6 +90,28 @@ module.exports = function (app, options) {
 
     });
 
+    // This is needed because when configuring a custom header JQuery automaticaly send options request to the server.
+    // That following line avoid cross domain error like
+    // XMLHttpRequest cannot load http://localhost.rts.ch:3000/api/local/info.
+    // Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+    // Origin 'http://localhost.rts.ch:8090' is therefore not allowed access.
+    app.options('/api/logout', cors);
+
+    app.get('/api/logout', cors, function (req, res, next) {
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        if (req.get('origin')) {
+            // Might not be needed... But needed sometime...
+            res.setHeader('Access-Control-Allow-Origin', req.get('origin'));
+        }
+        req.logout();
+
+        res.json({connected: false});
+
+    });
+
     function returnDisplayNameForUser(user, req, res) {
         if (!user) {
             // Return 200 to avoid error in browser console
