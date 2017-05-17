694427384da7e5f8cbf936b24c2b03227d695857
dom <dominique.chion@rts.ch>
2017-05-17 15:13:25 +0200

    Remove console.log

--- public/js/grecaptcha.js
+++ public/js/grecaptcha.js
@@ -1,12 +1,11 @@
 "use strict";
 
-function recaptchaReady () {
-    if($('#myrecaptcha').length) {
+function recaptchaReady() {
+    if ($('#myrecaptcha').length) {
         grecaptcha.render('myrecaptcha', {
             'sitekey': siteKey,
             'expired-callback': function () {
                 grecaptcha.reset();
-                console.log('recatpcha');
             }
         });
     }
