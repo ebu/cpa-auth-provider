59a4ffabd1729758f0f763713e7ba4d318c94fdf
Alexandre CAPELLARO <alexandre.capellaro@hortis.ch>
2017-08-30 15:04:08 +0200

    Good google files

--- routes/auth/google.js
+++ routes/auth/google.js
@@ -7,45 +7,88 @@ var requestHelper = require('../../lib/request-helper');
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20');
 
+function findOrCreateExternalUser(email, defaults) {
+    return new Promise(function (resolve, reject) {
+        db.User.find(
+            {
+                where: {
+                    email: email
+                }
+            }
+        ).then(
+            function (user) {
+                if (!user) {
+                    return db.User.findOrCreate(
+                        {
+                            where: {
+                                email: email
+                            },
+                            defaults: defaults
+                        }
+                    ).spread(
+                        function (user, created) {
+                            return resolve(user);
+                        }
+                    ).catch(reject);
+                }
+                if (!user.verified) {
+                    return resolve(false);
+                }
+                if (user.display_name) {
+                    defaults.display_name = user.display_name;
+                }
+                return user.updateAttributes(
+                    defaults
+                ).then(resolve, reject);
+            },
+            reject
+        );
+    });
+}
+
 passport.use(new GoogleStrategy({
         clientID: config.identity_providers.google.client_id,
-        clientSecret: config.identity_providers.google.client_secret
+        clientSecret: config.identity_providers.google.client_secret,
+        callbackURL: config.identity_providers.google.callback_url
     },
     function (accessToken, refreshToken, profile, done) {
-        var photo_url = (profile.photos.length > 0) ? profile.photos[0].value : null;
-        db.User.findOrCreate({
-            where: {
-                provider_uid: "fb:" + profile.id,
+        var email = '';
+        if (profile.emails !== undefined) {
+            email = profile.emails[0].value;
+        }
+
+        if (email === '') {
+            return done(new Error('NO_EMAIL', null));
+        }
+
+        var providerUid = 'google:' + profile.id;
+
+        return findOrCreateExternalUser(
+            email,
+            {
+                provider_uid: providerUid,
                 display_name: profile.displayName,
-                photo_url: photo_url
+                verified: true
             }
-        }).spread(function (user) {
-            user.logLogin().then(function () {
-            }, function () {
-            });
-            return done(null, user);
-        }).catch(function (err) {
-            done(err, null);
-        });
+        ).then(
+            function (u) {
+                if (u) {
+                    u.logLogin();
+                }
+                return done(null, u);
+            }
+        ).catch(
+            function (err) {
+                done(err);
+            }
+        );
     }
 ));
 
-// passport.use(new GooglePlusStrategy({
-//         clientId: 'YOUR_CLIENT_ID',
-//         clientSecret: 'YOUR_CLIENT_SECRET'
-//     },
-//     function(tokens, profile, done) {
-//         // Create or update user, call done() when complete...
-//         done(null, profile, tokens);
-//     }
-// ));
-
 module.exports = function (app, options) {
-    app.get('/auth/googleplus', passport.authenticate('facebook'));
+    app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
-    app.get('/auth/googleplus/callback', passport.authenticate('facebook', {
-        failureRedirect: '/?error=login_failed'
-    }), function (req, res, next) {
+    app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/auth?error=LOGIN_INVALID_EMAIL_BECAUSE_NOT_VALIDATED'}), function (req, res) {
 
         var redirectUri = req.session.auth_origin;
         delete req.session.auth_origin;
