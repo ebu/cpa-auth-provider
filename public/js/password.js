756d67544755d048a4d1b062ee91959d50776d9f
dom <dominique.chion@rts.ch>
2017-09-19 14:23:21 +0200

    i18n owasp password strenght

--- public/js/password.js
+++ public/js/password.js
@@ -9,7 +9,7 @@ function validatePassword(password) {
     var result = owaspPasswordStrengthTest.test(password);
 
     if (result.errors.length > 0 && password !== "") {
-        passwordLabel.innerHTML = translateOwasp(result.errors[0]);
+        passwordLabel.innerHTML = passStrenghtErrors[result.failedTests[0]] ;
         passwordLabel.style.display = "block";
     } else {
         passwordLabel.style.display = "none";
@@ -33,54 +33,4 @@ function setProgress(result) {
         progressBar.style.backgroundColor = "green";
     }
     progressBar.style.width = progress + "%";
-}
-
-window.OWASP_TRANSLATIONS = window.OWASP_TRANSLATIONS || {};
-
-var owaspPhrases = [
-    {
-        regex: /The password must be at least (\d+) characters long./,
-        name: 'OWASP_PASSWORD_LENGTH_MIN'
-    },
-    {
-        regex: /The password must be fewer than (\d+) characters./,
-        name: 'OWASP_PASSWORD_LENGTH_MAX'
-    },
-    {
-        regex: /The password may not contain sequences of three or more repeated characters./,
-        name: 'OWASP_PASSWORD_REPEAT_CHARACTER'
-    },
-    {
-        regex: /The password must contain at least one lowercase letter./,
-        name: 'OWASP_PASSWORD_REQ_LOWERCASE'
-    },
-    {
-        regex: /The password must contain at least one uppercase letter./,
-        name: 'OWASP_PASSWORD_REQ_UPPERCASE'
-    },
-    {
-        regex: /The password must contain at least one number./,
-        name: 'OWASP_PASSWORD_REQ_NUMBER'
-    },
-    {
-        regex: /The password must contain at least one special character./,
-        name: 'OWASP_PASSWORD_REQ_SPECIAL'
-    },
-];
-
-function translateOwasp(error) {
-    for (var i = 0; i < owaspPhrases.length; ++i) {
-        var test = owaspPhrases[i].regex.exec(error);
-        if (test) {
-            if (!window.OWASP_TRANSLATIONS.hasOwnProperty(owaspPhrases[i].name)) {
-                return error;
-            }
-            var result = window.OWASP_TRANSLATIONS[owaspPhrases[i].name];
-            if (test.length > 1) {
-                result = result.replace(/{}/g, test[1]);
-            }
-            return result;
-        }
-    }
-    return error;
 }
