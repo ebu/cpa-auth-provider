b485964e827d1e1775e696d25d370e6999d27a7d
dom <dominique.chion@rts.ch>
2017-09-29 13:07:18 +0200

    migration file

---
+++ migrate/migrations/20170929115500-extract-external-provider.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+    up: function (queryInterface, Sequelize) {
+        return new Promise(function (resolve, reject) {
+            queryInterface.removeColumn('Users', 'provider_uid').then(
+                function () {
+                    return queryInterface.createTable(
+                        'OAuthProviders',
+                        {
+                            id: {
+                                type: Sequelize.INTEGER,
+                                primaryKey: true,
+                                autoIncrement: true
+                            },
+                            createdAt: {
+                                type: Sequelize.DATE
+                            },
+                            updatedAt: {
+                                type: Sequelize.DATE
+                            },
+                            name: Sequelize.STRING,
+                            uid: Sequelize.STRING,
+                            //foreign key usage
+                            user_id: {
+                                type: Sequelize.INTEGER,
+                                references: {
+                                    model: 'Users',
+                                    key: 'id'
+                                }
+                            }
+                        });
+
+                }
+            ).then(resolve).catch(reject);
+        });
+    },
+
+    down: function (queryInterface, Sequelize) {
+        return new Promise(function (resolve, reject) {
+            queryInterface.removeColumn('UserProfiles', 'date_of_birth').then(
+                function () {
+                    return queryInterface.dropTable('OAuthProviders');
+                }
+            ).then(resolve).catch(reject);
+        });
+    }
+};
