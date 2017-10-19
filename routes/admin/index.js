760080477ed8d0895c714aa7e781c79128b16697
Daniel Krax <daniel.krax@br.de>
2017-10-19 18:00:42 +0200

    moved statistics to admin path

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -339,4 +339,43 @@ module.exports = function (router) {
         return toReturn;
     }
 
+
+    router.get(
+        '/admin/statistics',
+        [authHelper.ensureAuthenticated, permissionHelper.can(permissionName.ADMIN_PERMISSION)],
+        function (req, res) {
+            calculateValues().then(
+                values => {
+                    return res.status(200).json({summary: 'ok', values});
+                },
+                error => {
+                    return res.status(500).json({summary: 'error', message: error.message});
+                }
+            )
+        }
+    );
+
+
+    function calculateValues() {
+        let db = require('../../models');
+        return new Promise((resolve, reject) => {
+            let result = {accountsTotal: 0, accountsVerified: 0};
+            db.User.count().then(
+                c => {
+                    result.accountsTotal = c;
+                    return db.User.count({where: {'verified': true}});
+                }
+            ).then(
+                c => {
+                    result.accountsVerified = c;
+                    return resolve(result);
+                }
+            ).catch(
+                e => {
+                    return reject(e);
+                }
+            );
+        });
+    }
+
 };
--- routes/status.js
+++ routes/status.js
@@ -18,40 +18,4 @@ module.exports = function (router) {
             return res.status(200).json({summary: "ok"});
         }
     );
-
-    router.get(
-        '/statistics',
-        function (req, res) {
-            calculateValues().then(
-                values => {
-                    return res.status(200).json({summary: 'ok', values});
-                },
-                error => {
-                    return res.status(500).json({summary: 'error', message: error.message});
-                }
-            )
-        }
-    )
 };
-
-function calculateValues() {
-    let db = require('../models');
-    return new Promise((resolve, reject) => {
-        let result = {accountsTotal: 0, accountsVerified: 0};
-        db.User.count().then(
-            c => {
-                result.accountsTotal = c;
-                return db.User.count({where: {'verified': true}});
-            }
-        ).then(
-            c => {
-                result.accountsVerified = c;
-                return resolve(result);
-            }
-        ).catch(
-            e => {
-                return reject(e);
-            }
-        );
-    });
-}
