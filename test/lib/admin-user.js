675a5301fef7d75a8869060e5f58d66ce746dcd9
dom <dominique.chion@rts.ch>
2017-12-29 11:08:29 +0100

    55 failing tests...

--- test/lib/admin-user.js
+++ test/lib/admin-user.js
@@ -473,7 +473,9 @@ describe('GET /admin/users/csv', function () {
         expect(self.res.statusCode).to.equal(200);
         expect(self.res).to.have.header('content-disposition', 'attachment; filename=users.csv');
         expect(self.res).to.have.header('content-type', 'text/csv; charset=utf-8');
-        expect(self.res).to.have.header('content-length', '358'); // That check might fail if you update the format, so you'll have to check the new length make sens before updating it
+        expect(self.res.text).to.equal('id,email,firstname,lastname,permission_id,permission,created,password_changed,last_login\r\n' +
+            adminId + ',admin@admin.ch,John,Doe,1,admin,Sun Jan 29 2017 12:11:00 GMT+0100 (CET),Sun Jan 29 2017 12:11:00 GMT+0100 (CET),Sun Jan 29 2017 12:11:00 GMT+0100 (CET)\r\n' +
+            userId + ',user@user.ch,Scott,Tiger,,,Sun Jan 29 2017 12:11:00 GMT+0100 (CET),Sun Jan 29 2017 12:11:00 GMT+0100 (CET),\r\n');
     });
 
 
