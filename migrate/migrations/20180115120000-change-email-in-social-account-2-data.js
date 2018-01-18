89179ada7a7e28e13de2fd8a9c33597cc9a8cbc8
dom <dominique.chion@rts.ch>
2018-01-18 15:11:54 +0100

    WIP

--- migrate/migrations/20180115120000-change-email-in-social-account-2-data.js
+++ migrate/migrations/20180115120000-change-email-in-social-account-2-data.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//SQL Magic (POSTGRES) query to be able to run that migration file as much as you want :)
+// ﻿DELETE FROM public."SequelizeMeta" WHERE name='20180115120000-change-email-in-social-account-2-data.js';
+
 module.exports = {
     up: function (queryInterface, Sequelize) {
         return new Promise(function (resolve, reject) {
@@ -7,11 +10,14 @@ module.exports = {
             return queryInterface.sequelize.query("select * from \"public\".\"Users\"").then(function (users) {
                 var batch = [];
 
+                console.log("users", users);
+                console.log("coucou");
+                console.log("users[0]", users[0]);
                 // insert data in appropriate table
-                for (var i = 0; i < users.length; i++) {
-                    console.log("users[i]:", users[i]);
+                for (var i = 0; i < users[users.length - 1].rowCount; i++) {
+                    console.log("users.rows[0][" + i + "]:", users.rows[0][i]);
                     batch.push(queryInterface.sequelize.query("insert into \"public\".\"LocalLogins\" (login, password, verified, password_changed_at, last_login_at, user_id) " +
-                        " VALUES ('" + users[i].login + "', '" + users[i].password + "', '" + users[i].verified + "', '" + users[i].password_changed_at + "', '" + users[i].lastByte + "', '" + users[i].id + "')"));
+                        " VALUES ('" + users.rows[0][i].login + "', '" + users.rows[0][i].password + "', '" + users.rows[0][i].verified + "', '" + users.rows[0][i].password_changed_at + "', '" + users.rows[0][i].lastByte + "', '" + users.rows[0][i].id + "')"));
                 }
 
                 return Promise.all(batch);
