281f2af6678845e0739a17b82082c8494be45923
dom <dominique.chion@rts.ch>
2018-01-18 11:47:59 +0100

    WIP

--- migrate/migrations/20180115120000-change-email-in-social-account.js
+++ migrate/migrations/20180115120000-change-email-in-social-account.js
@@ -113,11 +113,11 @@ module.exports = {
                 });
                 // Move the data
             }).then(function () {
-                return queryInterface.sequelize.query("select * from Users");
+                return queryInterface.sequelize.query("select * from ﻿public.\"Users\""); //FIXME POSTGRES specific
             }).then(function (users) {
                 // insert data in appropriate table
                 for (var i = 0; i < users.length; i++) {
-                    queryInterface.sequelize.query("insert into LocalLogins (login, password, verified, password_changed_at, last_login_at, user_id) " +
+                    queryInterface.sequelize.query("insert into public.\"LocalLogins\" (login, password, verified, password_changed_at, last_login_at, user_id) " +
                         " VALUES ('" + users[i].login + "', " + users[i].password + "', " + users[i].verified + "', " + users[i].password_changed_at + "', " + users[i].lastByte + "', " + users[i].id + "')");
                 }
 
