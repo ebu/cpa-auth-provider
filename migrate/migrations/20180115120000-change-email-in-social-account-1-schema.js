0c8eb8ac5675d23ba582d75ac45ab919a4f87224
dom <dominique.chion@rts.ch>
2018-01-19 15:56:15 +0100

    missing column langage in social login table

--- migrate/migrations/20180115120000-change-email-in-social-account-1-schema.js
+++ migrate/migrations/20180115120000-change-email-in-social-account-1-schema.js
@@ -97,6 +97,15 @@ module.exports = {
                         allowNull: true
                     }
                 );
+            }).then(function () {
+                return queryInterface.addColumn(
+                    "SocialLogins",
+                    "language",
+                    {
+                        type: Sequelize.STRING,
+                        allowNull: true
+                    }
+                );
                 // Create table local login
             }).then(function () {
                 return queryInterface.createTable('LocalLogins', {
