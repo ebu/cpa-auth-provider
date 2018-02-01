d7cd75b2a2c4b1474b19ba857b3c21b509b5a3b6
dom <dominique.chion@rts.ch>
2018-02-01 15:38:09 +0100

    Seems to work :)

--- routes/api/facebook.js
+++ routes/api/facebook.js
@@ -7,45 +7,17 @@ var cors = require('../../lib/cors');
 var facebookHelper = require('../../lib/facebook-helper');
 
 module.exports = function (app, options) {
+
+    app.post('/oauth/facebook/signup', cors, function (req, res) {
+        if (!req.body.client_id) {
+            res.status(400).json({error: "Missing client id"});
+        } else {
+            facebookSignup(req, res);
+        }
+    });
+
     app.post('/api/facebook/signup', cors, function (req, res) {
-        var facebookAccessToken = req.body.fbToken;
-        if (facebookAccessToken && facebookAccessToken.length > 0) {
-            // Get back user object from Facebook
-            verifyFacebookUserAccessToken(facebookAccessToken, function (err, remoteProfile) {
-                if (remoteProfile) {
-                    // If the user already exists and his account is not validated
-                    // i.e.: there is a user in the database with the same id and this user email is not validated
-                    db.LocalLogin.find({
-                        where: {
-                            login: remoteProfile.email
-                        }
-                    }).then(function (localLoginInDb) {
-                        if (localLoginInDb && !localLoginInDb.verified) {
-                            res.status(400).json({error: req.__("LOGIN_INVALID_EMAIL_BECAUSE_NOT_VALIDATED_FB")});
-                        } else {
-                            socialLoginHelper.performLogin(remoteProfile, socialLoginHelper.FB, req.body.client_id, function (error, response) {
-
-                                if (response) {
-                                    res.status(200).json(response);
-                                } else {
-                                    res.status(500).json({error: error.message});
-                                }
-                            });
-                        }
-                    });
-
-                } else {
-                    res.status(401).json({error: "No valid facebook profile found"});
-                }
-
-            });
-        }
-        else {
-            // 400 BAD REQUEST
-            console.log('error', 'Bad login request from ' +
-                req.connection.remoteAddress + '. Reason: facebook access token and application name are required.');
-            res.status(400);
-        }
+        facebookSignup(req, res);
     });
 };
 
@@ -54,11 +26,52 @@ function verifyFacebookUserAccessToken(token, done) {
     request(path, function (error, response, body) {
         var data = JSON.parse(body);
         if (!error && response && response.statusCode && response.statusCode === 200) {
-            var remoteProfile = socialLoginHelper.buildRemoteProfile(facebookHelper.buildFBId(data.id), data.name,data.email,data.first_name,data.last_name,data.gender,facebookHelper.fbDateOfBirthToTimestamp(data.birthday));
+            var remoteProfile = socialLoginHelper.buildRemoteProfile(facebookHelper.buildFBId(data.id), data.name, data.email, data.first_name, data.last_name, data.gender, facebookHelper.fbDateOfBirthToTimestamp(data.birthday));
             done(null, remoteProfile);
         } else {
             done({code: response.statusCode, message: data.error.message}, null);
         }
     });
+}
+
+function facebookSignup(req, res) {
+    var facebookAccessToken = req.body.fbToken;
+    if (facebookAccessToken && facebookAccessToken.length > 0) {
+        // Get back user object from Facebook
+        verifyFacebookUserAccessToken(facebookAccessToken, function (err, remoteProfile) {
+            if (remoteProfile) {
+                // If the user already exists and his account is not validated
+                // i.e.: there is a user in the database with the same id and this user email is not validated
+                db.LocalLogin.find({
+                    where: {
+                        login: remoteProfile.email
+                    }
+                }).then(function (localLoginInDb) {
+                    if (localLoginInDb && !localLoginInDb.verified) {
+                        res.status(400).json({error: req.__("LOGIN_INVALID_EMAIL_BECAUSE_NOT_VALIDATED_FB")});
+                    } else {
+                        socialLoginHelper.performLogin(remoteProfile, socialLoginHelper.FB, req.body.client_id, function (error, response) {
+
+                            if (response) {
+                                res.status(200).json(response);
+                            } else {
+                                res.status(500).json({error: error.message});
+                            }
+                        });
+                    }
+                });
+
+            } else {
+                res.status(401).json({error: "No valid facebook profile found"});
+            }
+
+        });
+    }
+    else {
+        // 400 BAD REQUEST
+        console.log('error', 'Bad login request from ' +
+            req.connection.remoteAddress + '. Reason: facebook access token and application name are required.');
+        res.status(400);
+    }
 }
 
--- routes/api/google.js
+++ routes/api/google.js
@@ -6,45 +6,58 @@ var cors = require('../../lib/cors');
 var googleHelper = require('../../lib/google-helper');
 
 module.exports = function (app, options) {
-    app.post('/api/google/signup', cors, function (req, res) {
-            var googleIdToken = req.body.idToken;
-            var remoteProfile;
 
-            if (googleIdToken && googleIdToken.length > 0) {
-                googleHelper.verifyGoogleIdToken(googleIdToken).then(
-                    function (googleProfile) {
-                        // If the googleProfile already exists and his account is not validated
-                        // i.e.: there is a user in the database with the same id and this user email is not validated
-                        remoteProfile = socialLoginHelper.buildRemoteProfile(googleHelper.buildGoogleId(googleProfile.provider_uid), googleProfile.display_name, googleProfile.email, googleProfile.givenName, googleProfile.familyName, googleProfile.gender, null);
-                        return db.LocalLogin.findOne({
-                            where: {
-                                login: googleProfile.email
-                            }
-                        });
-                    }
-                ).then(
-                    function (localLoginInDb) {
-                        if (localLoginInDb && !localLoginInDb.verified) {
-                            res.status(400).json({error: req.__("LOGIN_INVALID_EMAIL_BECAUSE_NOT_VALIDATED_GOOGLE")});
-                        } else {
-                            socialLoginHelper.performLogin(remoteProfile, socialLoginHelper.GOOGLE, req.body.client_id, function (error, response) {
-                                if (response) {
-                                    res.status(200).json(response);
-                                } else {
-                                    res.status(500).json({error: error.message});
-                                }
-                            });
-                        }
-                    }
-                ).catch(
-                    function (error) {
-                        res.status(500).json({error: error.message});
-                    }
-                );
-            }
-            else {
-                res.status(400).json({error: 'Missing google IDtoken to connect with Google account'});
-            }
+    app.post('/oauth/google/signup', cors, function (req, res) {
+        if (!req.body.client_id) {
+            res.status(400).json({error: "Missing client id"});
+        } else {
+            googleSignup(req, res);
         }
-    );
+    });
+
+    app.post('/api/google/signup', cors, function (req, res) {
+        googleSignup(req, res);
+    });
 };
+
+function googleSignup(req, res) {
+    var googleIdToken = req.body.idToken;
+    var remoteProfile;
+
+    if (googleIdToken && googleIdToken.length > 0) {
+        googleHelper.verifyGoogleIdToken(googleIdToken).then(
+            function (googleProfile) {
+                // If the googleProfile already exists and his account is not validated
+                // i.e.: there is a user in the database with the same id and this user email is not validated
+                remoteProfile = socialLoginHelper.buildRemoteProfile(googleHelper.buildGoogleId(googleProfile.provider_uid), googleProfile.display_name, googleProfile.email, googleProfile.givenName, googleProfile.familyName, googleProfile.gender, null);
+                return db.LocalLogin.findOne({
+                    where: {
+                        login: googleProfile.email
+                    }
+                });
+            }
+        ).then(
+            function (localLoginInDb) {
+                if (localLoginInDb && !localLoginInDb.verified) {
+                    res.status(400).json({error: req.__("LOGIN_INVALID_EMAIL_BECAUSE_NOT_VALIDATED_GOOGLE")});
+                } else {
+                    socialLoginHelper.performLogin(remoteProfile, socialLoginHelper.GOOGLE, req.body.clientId, function (error, response) {
+                        if (response) {
+                            res.status(200).json(response);
+                        } else {
+                            res.status(500).json({error: error.message});
+                        }
+                    });
+                }
+            }
+        ).catch(
+            function (error) {
+                res.status(500).json({error: error.message});
+            }
+        );
+    }
+    else {
+        res.status(400).json({error: 'Missing google IDtoken to connect with Google account'});
+    }
+}
+
