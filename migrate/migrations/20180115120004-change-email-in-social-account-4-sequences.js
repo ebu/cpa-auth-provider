50406337a7673676ae4cb2c9b1915ab1242683b4
dom <dominique.chion@rts.ch>
2018-02-12 15:59:56 +0100

    Shorter seq restart

--- migrate/migrations/20180115120004-change-email-in-social-account-4-sequences.js
+++ migrate/migrations/20180115120004-change-email-in-social-account-4-sequences.js
@@ -5,17 +5,8 @@ module.exports = {
         return new Promise(function (resolve, reject) {
             if (process.env.DB_TYPE === "postgres") {
                 // max from table User:
-                return queryInterface.sequelize.query("select max(id) from Users").then(function (maxUserIdRes) {
-                    // udpate sequence:
-                    console.log("maxUserIdRes", maxUserIdRes);
-                    return queryInterface.sequelize.query("Users_id_seq\" RESTART WITH " + (maxUserIdRes[0]) + 1);
-                }).then(function () {
-                    // max from table LocalLogin:
-                    return queryInterface.sequelize.query("select max(id) from LocalLogins");
-                }).then(function (maxLocalLoginIdRes) {
-                    // udpate sequence:
-                    console.log("maxLocalLoginIdRes", maxLocalLoginIdRes);
-                    return queryInterface.sequelize.query("ALTER SEQUENCE \"LocalLogins_id_seq\" RESTART WITH " + (maxLocalLoginIdRes[0]) + 1);
+                return queryInterface.sequelize.query("ALTER SEQUENCE public.\"Users_id_seq\" RESTART").then(function () {
+                    return queryInterface.sequelize.query("ALTER SEQUENCE \"LocalLogins_id_seq\" RESTART");
                 }).then(function () {
                     resolve();
                 }).catch(reject);
