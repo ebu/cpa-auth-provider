ff1b54519e38a1c284195daf45f72889d0c0a193
dom <dominique.chion@rts.ch>
2018-02-12 16:40:38 +0100

    Done :)

--- migrate/migrations/20180115120004-change-email-in-social-account-4-sequences.js
+++ migrate/migrations/20180115120004-change-email-in-social-account-4-sequences.js
@@ -3,31 +3,28 @@
 module.exports = {
     up: function (queryInterface, Sequelize) {
         return new Promise(function (resolve, reject) {
-        }).then(function () {
+            console.log("go...");
             if (process.env.DB_TYPE === "postgres") {
+                console.log("postgres...");
                 // rename sequence
                 // max from table User:
-                return queryInterface.sequelize.query("select max(id) from Users").then(function (maxUserIdRes) {
+                return queryInterface.sequelize.query("select max(id) from public.\"Users\"").then(function (maxUserIdRes) {
                     // udpate sequence:
-                    console.log("maxUserIdRes", maxUserIdRes);
-                    let max = maxUserIdRes[0][0];
-                    console.log("maxUserIdRes", max);
+                    let max = maxUserIdRes[0][0].max;
                     if (!max) {
                         max = 0;
                     }
-                    return queryInterface.sequelize.query("Users_id_seq\" RESTART WITH " + (max + 1));
+                    return queryInterface.sequelize.query("ALTER  SEQUENCE public.\"Users_id_seq\" RESTART WITH " + (max + 1));
                 }).then(function () {
                     // max from table LocalLogin:
-                    return queryInterface.sequelize.query("select max(id) from LocalLogins");
+                    return queryInterface.sequelize.query("select max(id) from public.\"LocalLogins\"");
                 }).then(function (maxLocalLoginIdRes) {
                     // udpate sequence:
-                    console.log("maxLocalLoginIdRes", maxLocalLoginIdRes);
-                    let max = maxLocalLoginIdRes[0][0];
-                    console.log("maxUserIdRes", max);
+                    let max = maxLocalLoginIdRes[0][0].max;
                     if (!max) {
                         max = 0;
                     }
-                    return queryInterface.sequelize.query("ALTER SEQUENCE \"LocalLogins_id_seq\" RESTART WITH " + (max + 1));
+                    return queryInterface.sequelize.query("ALTER SEQUENCE public.\"LocalLogins_id_seq\" RESTART WITH " + (max + 1));
                 }).then(function () {
                     resolve();
                 }).catch(reject);
