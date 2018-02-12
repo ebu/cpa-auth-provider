16947bf69eea0fee593bf4865c1c9ca4c5c64422
dom <dominique.chion@rts.ch>
2018-02-12 15:27:11 +0100

    Seq migration in dedicated file

---
+++ migrate/migrations/20180115120004-change-email-in-social-account-4-sequences.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+    up: function (queryInterface, Sequelize) {
+        return new Promise(function (resolve, reject) {
+            if (process.env.DB_TYPE === "postgres") {
+                // rename sequence
+                return queryInterface.sequelize.query("ALTER SEQUENCE TODO RENAME TO LocalLogins_id_seq").then(function () {
+                    // max from table User:
+                    return queryInterface.sequelize.query("select max(id) from Users");
+                }).then(function (maxUserIdRes) {
+                    // udpate sequence:
+                    console.log("maxUserIdRes", maxUserIdRes);
+                    return queryInterface.sequelize.query("Users_id_seq\" RESTART WITH " + (maxUserIdRes[0]) + 1);
+                }).then(function () {
+                    // max from table LocalLogin:
+                    return queryInterface.sequelize.query("select max(id) from LocalLogins");
+                }).then(function (maxLocalLoginIdRes) {
+                    // udpate sequence:
+                    console.log("maxLocalLoginIdRes", maxLocalLoginIdRes);
+                    return queryInterface.sequelize.query("ALTER SEQUENCE \"LocalLogins_id_seq\" RESTART WITH " + (maxLocalLoginIdRes[0]) + 1);
+                }).then(function () {
+                    resolve();
+                }).catch(reject);
+            } else {
+                resolve();
+            }
+        });
+    },
+
+    down: function (queryInterface, Sequelize) {
+        return new Promise(function (resolve, reject) {
+            resolve();
+        });
+    }
+}
