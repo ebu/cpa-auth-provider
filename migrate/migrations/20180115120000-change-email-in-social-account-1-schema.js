d402e42cbc95bed08115d1e6d608302df08a3da5
dom <dominique.chion@rts.ch>
2018-02-12 14:30:27 +0100

    temp log

--- migrate/migrations/20180115120000-change-email-in-social-account-1-schema.js
+++ migrate/migrations/20180115120000-change-email-in-social-account-1-schema.js
@@ -154,12 +154,14 @@ module.exports = {
                         return queryInterface.sequelize.query("select max(id) from Users");
                     }).then(function (maxUserIdRes) {
                         // udpate sequence:
+                        console.log("maxUserIdRes", maxUserIdRes);
                         return queryInterface.sequelize.query("Users_id_seq\" RESTART WITH " + (maxUserIdRes[0]) + 1);
                     }).then(function () {
                         // max from table LocalLogin:
                         return queryInterface.sequelize.query("select max(id) from LocalLogins");
                     }).then(function (maxLocalLoginIdRes) {
                         // udpate sequence:
+                        console.log("maxLocalLoginIdRes", maxLocalLoginIdRes);
                         return queryInterface.sequelize.query("ALTER SEQUENCE \"LocalLogins_id_seq\" RESTART WITH " + (maxLocalLoginIdRes[0]) + 1);
                     }).then(function () {
                         resolve();
