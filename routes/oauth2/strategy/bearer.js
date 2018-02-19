34abf1d2e69ee51fdb48753a792a0443cacc142b
Daniel Krax <daniel.krax@br.de>
2018-02-19 13:53:26 +0100

    cleanup

--- routes/oauth2/strategy/bearer.js
+++ routes/oauth2/strategy/bearer.js
@@ -24,50 +24,21 @@ exports.bearer = new BearerStrategy(function (accessToken, done) {
                 }
             }
             if (userId) {
-                db.User.find({where: {id: userId}, include: db.LocalLogin}).then(
-                    function (user) {
+                db.User.findOne({where: {id: userId}, include: db.LocalLogin}).then(
+                    user => {
                         if (!user) {
                             return done(null, false);
                         }
                         // TODO: Define scope
-                        var info = {scope: '*', client: client};
+                        const info = {scope: '*', client: client};
                         return done(null, user, info);
                     }
                 ).catch(done);
             } else {
                 // TODO: Define scope
-                var info = {scope: '*', client: client};
+                const info = {scope: '*', client: client};
                 return done(null, client, info);
             }
         }
     ).catch(done);
-
-    //
-    // db.AccessToken.find({where: {token: accessToken}}).then(function (token) {
-    //     if (!token) {
-    //         return done(null, false);
-    //     }
-    //
-    //     if (token.user_id !== null) {
-    //         db.User.findOne({where: {id: token.user_id}}).then(function (user) {
-    //             if (!user) {
-    //                 return done(null, false);
-    //             }
-    //             // TODO: Define scope
-    //             var info = {scope: '*'};
-    //             done(null, user, info);
-    //         }).catch(done);
-    //     } else {
-    //         //The request came from a client only since userID is null
-    //         //therefore the client is passed back instead of a user
-    //         db.OAuth2Client.findOne({where: {id: token.client_id}}).then(function (client) {
-    //             if (!client) {
-    //                 return done(null, false);
-    //             }
-    //             // TODO: Define scope
-    //             var info = {scope: '*'};
-    //             done(null, client, info);
-    //         }).catch(done);
-    //     }
-    // }).catch(done);
 });
