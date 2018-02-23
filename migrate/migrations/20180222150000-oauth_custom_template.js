c9e5626e21feb4c3f6a39dcb0dd131afb0d98c00
dom <dominique.chion@rts.ch>
2018-02-23 10:22:34 +0100

    Fix migration

--- migrate/migrations/20180222150000-oauth_custom_template.js
+++ migrate/migrations/20180222150000-oauth_custom_template.js
@@ -15,13 +15,6 @@ module.exports = {
 
     down: function (queryInterface, Sequelize) {
         // Add new columns to table Users
-        return queryInterface.removeColumn(
-            "OAuth2Client",
-            "use_template",
-            {
-                type: Sequelize.STRING,
-                allowNull: true
-            }
-        );
+        return queryInterface.removeColumn("OAuth2Client", "use_template");
     }
 }
