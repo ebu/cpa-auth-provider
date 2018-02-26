4a8e476ba99e4d702bfa552a702672ebf295f3c9
dom <dominique.chion@rts.ch>
2018-02-26 12:08:37 +0100

    attempt to fix warning

--- routes/oauth2/strategy/bearer.js
+++ routes/oauth2/strategy/bearer.js
@@ -7,7 +7,7 @@ var logger = require('../../../lib/logger');
 
 exports.bearer = new BearerStrategy(function (accessToken, done) {
     var clientId = jwtHelper.read(accessToken).cli;
-    db.OAuth2Client.find({where: {id: clientId}}).then(
+    return db.OAuth2Client.find({where: {id: clientId}}).then(
         function (client) {
             if (!client) {
                 return done(null, false);
@@ -24,7 +24,7 @@ exports.bearer = new BearerStrategy(function (accessToken, done) {
                 }
             }
             if (userId) {
-                db.User.findOne({where: {id: userId}, include: db.LocalLogin}).then(
+                return db.User.findOne({where: {id: userId}, include: db.LocalLogin}).then(
                     user => {
                         if (!user) {
                             return done(null, false);
@@ -33,12 +33,20 @@ exports.bearer = new BearerStrategy(function (accessToken, done) {
                         const info = {scope: '*', client: client};
                         return done(null, user, info);
                     }
-                ).catch(done);
+                ).catch(function (err) {
+                        logger.info('[OAuth2][Info][Error', err, ']');
+                        return done();
+                    }
+                );
             } else {
                 // TODO: Define scope
                 const info = {scope: '*', client: client};
                 return done(null, client, info);
             }
         }
-    ).catch(done);
+    ).catch(function (err) {
+            logger.info('[OAuth2][Info][Unexpected error', err, ']');
+            return done();
+        }
+    );
 });
